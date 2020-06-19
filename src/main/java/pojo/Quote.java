1b96a2a8ed43d1d12a52e6d298334612dd24ce7a
dikshatiwari14 <diksha.tiwari14@gmail.com>
2020-06-19 23:50:27 +0530

    added Quote.java, QuoteDetails.java, SessionDetails.java

---
+++ src/main/java/pojo/Quote.java
@@ -0,0 +1,21 @@
+package pojo;
+
+public class Quote {
+	
+	private String author;
+	private String body;
+	
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getBody() {
+		return body;
+	}
+	public void setBody(String body) {
+		this.body = body;
+	}
+
+}
---
+++ src/main/java/pojo/QuoteDetails.java
@@ -0,0 +1,19 @@
+package pojo;
+
+public class QuoteDetails {
+	
+	private Quote quote;
+
+	public Quote getQuote() {
+		return quote;
+	}
+
+	public void setQuote(Quote quote) {
+		this.quote = quote;
+	}
+
+
+	
+	
+
+}
